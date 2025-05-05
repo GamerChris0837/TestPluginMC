7aca0eef032dd206755dc04a64eeea0a2386466a
GamerChris0837 <jjangga4227@gmail.com>
2025-05-06 01:17:07 +0900

    d

--- src/main/java/com/GChr1s/TestPluginMC/TestPluginMC.java
+++ src/main/java/com/GChr1s/TestPluginMC/TestPluginMC.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TestPluginMC extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("플러그인 활성화됨!");
+        getLogger().info("Plugin is now on da!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("플러그인 비활성화됨.");
+        getLogger().info("Plugin is now off da.");
     }
 }
